63cf039836959289aa65ae137ab07bd1ca842b91
Ismael S. Souza <maelsilva96@gmail.com>
2023-02-24 04:20:55 -0300

    Alteração da forma que passamos as headers

--- apigateway/create_response.go
+++ apigateway/create_response.go
@@ -8,13 +8,37 @@ import (
 	"net/http"
 )
 
-func CreateResponse(statusCode int, headers map[string]string, body entities.ResponseBody) events.APIGatewayProxyResponse {
+type Response struct {
+	headers map[string]string
+}
+
+func NewResponse(httpMethod string) *Response {
+	return &Response{
+		headers: map[string]string{
+			"Strict-Transport-Security":      "max-age=31536000; includeSubDomains; preload",
+			"X-Content-Type-Options":         "nosniff",
+			"X-Frame-Options":                "SAMEORIGIN",
+			"Access-Control-Allow-Origin":    "*",
+			"Access-Control-Request-Method:": httpMethod,
+		},
+	}
+}
+
+func (resp *Response) AddHeader(key string, val string) {
+	resp.headers[key] = val
+}
+
+func (resp *Response) RemoveHeader(key string) {
+	delete(resp.headers, key)
+}
+
+func (resp *Response) CreateResponse(statusCode int, body entities.ResponseBody) events.APIGatewayProxyResponse {
 	data, err := json.Marshal(body)
 	if err != nil {
 		log.Println(err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
-			Headers:    headers,
+			Headers:    resp.headers,
 			Body:       "{\"success\":false,\"message\":\"Internal error!\",\"error_code\":1}",
 		}
 	}
