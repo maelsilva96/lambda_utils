4ff528fa40470adbd89d5b11d547b99e8226fbb0
Ismael S. Souza <maelsilva96@gmail.com>
2023-03-01 21:16:28 -0300

    Configuração do novo código de erro

--- entities/field_error.go
+++ entities/field_error.go
@@ -3,15 +3,16 @@ package entities
 type ErrorCode int
 
 const (
-	None               ErrorCode = 0
-	UnMappedCode       ErrorCode = 1
-	NoAuthorizedCode   ErrorCode = 2
-	ResetPasswordCode  ErrorCode = 3
-	FieldErrorCode     ErrorCode = 4
-	ChangePasswordCode ErrorCode = 5
-	ExpiredCode        ErrorCode = 6
-	TokenNotFoundCode  ErrorCode = 7
-	ExistingUser       ErrorCode = 8
+	None                ErrorCode = 0
+	UnMappedCode        ErrorCode = 1
+	NoAuthorizedCode    ErrorCode = 2
+	ResetPasswordCode   ErrorCode = 3
+	FieldErrorCode      ErrorCode = 4
+	ChangePasswordCode  ErrorCode = 5
+	ExpiredCode         ErrorCode = 6
+	TokenNotFoundCode   ErrorCode = 7
+	ExistingUser        ErrorCode = 8
+	InvalidPasswordCode ErrorCode = 9
 )
 
 type FieldError struct {
